10c3d2c3227a094992eab849b5da35d907817e4e
ElonJelinek <vsngvbey@gmail.com>
2023-04-24 22:58:45 +0800

    update

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://github.com/ElonJelinek/chatgpt-web/blob/main/src/assets/ae.jpg',
+      name: 'ElonJelinek',
+      description: 'Star on <a href="https://github.com/ElonJelinek/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
